b9019de20083819e4f1f932900ace35a593136a4
Jorge Rodriguez <jorge.rodriguez.suarez@gmail.com>
2011-03-31 22:23:50 -0400

    Internacionalizacion, manejo de errores, botones fb, twitter

--- media/js/main.js
+++ media/js/main.js
@@ -1,19 +1,11 @@
 $(document).ready(function(){
-	var id = "#register";
-	$("#action-open-register").click(function(e){
-		//Cancel the link behavior
-		e.preventDefault();		
+	var id = "#msg";
 
+	function show(id) {
+		
 		//Get the screen height and width
 		var maskHeight = $(document).height();
 		var maskWidth = $(window).width();
-
-		//Set heigth and width to mask to fill up the whole screen
-		$('#mask').css({'width':maskWidth,'height':maskHeight});
-
-		//transition effect		
-		$('#mask').fadeTo("slow",0.8);	
-
 		//Get the window height and width
 		var winH = $(window).height();
 		var winW = $(window).width();
@@ -21,13 +13,27 @@ $(document).ready(function(){
 		console.log($(id).height());
 		console.log( winH/2-$(id).height()/2);
 
+		//Set heigth and width to mask to fill up the whole screen
+		$('#mask').css({'width':maskWidth,'height':maskHeight});
+		
+		//transition effect		
+		$('#mask').fadeTo("slow",0.8);
+
 		//Set the popup window to center
 		$(id).css('top',  winH/2-$(".modal-border").height()/2);
 		$(id).css('left', (winW/2-$(".modal-border").width()/2)-120);
 
 		//transition effect
 		$(id).fadeIn(100);
+	}
+	
+	$("#action-open-register").click(function(e){
+		//Cancel the link behavior
+		e.preventDefault();
+		id = "#register";
+		show(id);
 	});
+	
 	//if close button is clicked
 	$('.action-close-register').click(function (e) {
 		//Cancel the link behavior
@@ -42,4 +48,8 @@ $(document).ready(function(){
 		$(this).hide();
 		$(id).hide();
 	});
+	
+	id = "#msg";
+	if ($(id).size()) show(id);
+	
 });
