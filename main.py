4e4e1bac0cb9150d2a34b4bba6df377ccc0dccad
Rodrigo Augosto <rodrigo.augosto@gmail.com>
2011-05-16 00:30:16 -0400

    adds country to register information Better counter view

--- main.py
+++ main.py
@@ -24,22 +24,29 @@ from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.ext.webapp \
 	import util, template
-	
+
 from string import *
 
 import logging
+
+def get_country(self):
+	try:
+		raw = urlfetch.fetch("http://api.hostip.info/get_html.php?ip="+self.request.remote_addr)
+	except:
+		return "XX"
+	country = ""
+	try:
+		country = raw.content.split('(')[1]
+		country = country.split(')')[0]
+	except:
+		country = "XX"
+	return country
 
 def isAddressValid(email):
 	if len(email) > 7:
 		if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
 			return 1
 	return 0
-
-class Register(db.Model):
-  email = db.StringProperty(required=True)
-  when = db.DateTimeProperty(auto_now_add=True)
-  remote_addr = db.StringProperty(required=True)
-  language = db.StringProperty()
 
 def get_device(self):
 	uastring = self.request.user_agent
@@ -55,7 +62,7 @@ def set_lang_cookie_and_return_dict(request, response):
 	else:
 		# set cookie to en
 		lang_cookie = request.get("hl")
-	
+
 	response.headers.add_header("Set-Cookie", "hl=" + lang_cookie + ";")
 	lang = {
 	  'en': languages.en,
@@ -63,6 +70,13 @@ def set_lang_cookie_and_return_dict(request, response):
 	  'pt': languages.pt
 	}[lang_cookie]
 	return lang
+
+class Register(db.Model):
+	email = db.StringProperty(required=True)
+	when = db.DateTimeProperty(auto_now_add=True)
+	remote_addr = db.StringProperty(required=True)
+	language = db.StringProperty()
+	country = db.StringProperty()
 
 def we_are():
 	return db.GqlQuery(
@@ -86,16 +100,16 @@ class MainHandler(webapp.RequestHandler):
 			template.render('index.html', params))
 
 class RegisterHandler(webapp.RequestHandler):
-	
+
 	def get(self):
 		self.redirect("/")
-		
+
 	def post(self):
 		# ***** Damain Control *****
 		if self.request.referer.find("http://localhost") == -1 and self.request.referer.find("http://www.startechconf.com/") == -1:
 			self.redirect("/?error=fake")
 			return
-			
+
 		# ***** Define some variables *****
 		lang = set_lang_cookie_and_return_dict(self.request, self.response)
 		ip = self.request.remote_addr
@@ -103,13 +117,13 @@ class RegisterHandler(webapp.RequestHandler):
 		email = self.request.get("email")
 		challenge = self.request.get('recaptcha_challenge_field')
 		response  = self.request.get('recaptcha_response_field')
-				
+
 		cResponse = captcha.submit(
-		                 challenge,
-		                 response.encode('utf-8'),
-		                 "6Lc_FsMSAAAAAEeoIjOaGU_M0obCkgDPbIevfUUV",
-		                 ip)
-		
+						 challenge,
+						 response.encode('utf-8'),
+						 "6Lc_FsMSAAAAAEeoIjOaGU_M0obCkgDPbIevfUUV",
+						 ip)
+
 		logging.info(cResponse.error_code)
 		if not cResponse.is_valid:
 			params = {
@@ -123,7 +137,7 @@ class RegisterHandler(webapp.RequestHandler):
 			self.response.out.write(
 				template.render('index.html', params))
 			return
-	
+
 		logging.info(email)
 		# ***** Email Verification *****
 		if not isAddressValid(email):
@@ -137,7 +151,7 @@ class RegisterHandler(webapp.RequestHandler):
 			}
 			self.response.out.write(
 				template.render('index.html', params))
-			
+
 		else:
 			registers = db.GqlQuery(
 				"SELECT * FROM Register "
@@ -158,34 +172,35 @@ class RegisterHandler(webapp.RequestHandler):
 				register = Register(
 					email = email,
 					remote_addr = ip,
-					language = lang["id"]
+					language = lang["id"],
+					country = get_country(self)
 				)
 				register.put()
-				
+
 				# Internal
 				message_to_admin = mail.EmailMessage()
 				message_to_admin.sender = "contact@startechconf.com"
 				message_to_admin.subject = "StarTechConf - Preregister"
 				message_to_admin.to = "rodrigo.augosto@gmail.com, contact@startechconf.com"
 				message_to_admin.body = '{\n\t"email": "%(email)s", \n\t"when": "%(when)s", \n\t"remote_addr": "%(remote_addr)s", \n\t"language": "%(language)s"\n},' % \
-				          {'email': email, "when": str(now), "remote_addr": ip, "language": lang["id"]}
+						  {'email': email, "when": str(now), "remote_addr": ip, "language": lang["id"]}
 				message_to_admin.send()
-		
-				
+
+
 				#External
 				message_to_user = mail.EmailMessage()
 				message_to_user.sender = "contact@startechconf.com"
 				message_to_user.subject = lang["registered_email_subject"]
-				
+
 				logging.info(message_to_user.subject)
-				
+
 				message_to_user.to = email
 				message_to_user.body = lang["registered_email_body"]
-				
+
 				logging.info(message_to_user.body)
-				
+
 				message_to_user.send()
-				
+
 				params = {
 					'device': get_device(self),
 					'path' : self.request.path,
@@ -206,20 +221,29 @@ class OrganizersHandler(webapp.RequestHandler):
 			'lang': set_lang_cookie_and_return_dict(self.request, self.response)
 		}		
 		self.response.out.write(template.render('organizers.html', params))
-		
+
 class Counter(webapp.RequestHandler):
 	def get(self):
-		self.response.out.write("<h1 style=\"font-size: 5em;\">"+str(we_are().count())+"</h1>")
+		user = users.get_current_user()
+		if user:
+			greeting = ("Welcome, %s! <a href=\"%s\">sign out</a><h1 style=\"font-size: 5em;\">we are: %s</h1>" %
+						(user.nickname(), users.create_logout_url(self.request.path), str(we_are().count())))
+		else:
+			greeting = ("<a href=\"%s\">Sign in or register</a>." %
+						users.create_login_url(self.request.path))
+
+		self.response.out.write("<html><body>%s</body></html>" %
+								(greeting))
 
 def main():
-    application = webapp.WSGIApplication([
+	application = webapp.WSGIApplication([
 		('/', MainHandler),
 		('/[R|r]egister', RegisterHandler),
 		('/counter', Counter),
 		#('/[O|o]rganizers', OrganizersHandler),
 	], debug=False)
-    util.run_wsgi_app(application)
+	util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+	main()
