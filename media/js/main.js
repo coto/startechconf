0eb616510aa299b68a0b547ec42a8159d92754ea
Rodrigo Augosto <rodrigo.augosto@gmail.com>
2011-06-16 01:32:54 -0400

    remove pre-register functionality

--- media/js/main.js
+++ media/js/main.js
@@ -2,48 +2,4 @@ $(document).ready(function(){
     twttr.anywhere(function (T) {
         T("#follow-placeholder").followButton('startechconf');
     });
-
-	function show(id) {
-		//Get the screen height and width
-		var maskHeight = $(document).height();
-		var maskWidth = $(window).width();
-		//Get the window height and width
-		var winH = $(window).height();
-		var winW = $(window).width();
-
-		//Set heigth and width to mask to fill up the whole screen
-		$('#mask').css({'width':maskWidth,'height':maskHeight});
-		
-		//transition effect		
-		$('#mask').fadeTo("slow",0.8);
-
-		//Set the popup window to center
-		$(id).css('top',  winH/2-$(".modal-border").height()/2);
-		$(id).css('left', (winW/2-$(".modal-border").width()/2)-120);
-
-		//transition effect
-		$(id).fadeIn(100);
-	}
-    var id = "#msg";
-
-	$("#action-open").click(function(e){
-		e.preventDefault();
-        id = $(this).attr("rel");
-        show(id);
-	});
-	
-	$('.action-close').click(function (e) {
-		e.preventDefault();
-
-		$('#mask').hide();
-		$($(this).attr("rel")).hide();
-	});		
-
-	$('#mask').click(function () {
-		$(this).hide();
-		$(id).hide();
-	});
-	
-	if ($(id).size()) show(id);
-
 });
