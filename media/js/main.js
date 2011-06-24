b04260eff4dffda16c280ddd1e9bd0fd18b780b2
Rodrigo Augosto <rodrigo.augosto@gmail.com>
2011-06-24 01:30:17 -0400

    new way to show "choose your language"

--- media/js/main.js
+++ media/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     twttr.anywhere(function (T) {
         T("#follow-placeholder").followButton('startechconf');
     });
+    $("#selectlanguage").change(function(){
+        newLang = $(this).val();
+        if ($(this).attr("current") == newLang)
+            return;
+        location.href = "/?hl="+newLang;
+    });
 });
