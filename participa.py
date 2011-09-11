f1106716c48fb571b4737b1cf24c8f70e31c0c7d
Rodrigo Augosto <rodrigo.augosto@gmail.com>
2011-09-11 04:35:43 -0300

    participa 1st version

--- participa.py
+++ participa.py
@@ -93,6 +93,7 @@ class Posts(db.Model):
     msj = db.TextProperty(required=True)
     fecha = db.DateTimeProperty(auto_now_add=True)
     network = db.StringProperty(required=True, default="null")
+    enabled = db.BooleanProperty(default=True)
 
 class OAuthToken(db.Model):
     token_key = db.StringProperty(required=True)
@@ -156,178 +157,164 @@ class TwitterLogout(RequestHandler):
         self.redirect('/participa')
 
 class HomeHandler(BaseHandler):
-	def slugify(s):
-		return ''.join(unicodedata.normalize("NFD",c)[0] for c in s)
+    def slugify(s):
+        return ''.join(unicodedata.normalize("NFD",c)[0] for c in s)
 
-	@property
-	def current_user_twitter(self):
-		self._current_user_twitter = None
-		oauth_token = self.request.get("oauth_token")
-		oauth_verifier = self.request.get("oauth_verifier")
-		if oauth_token is None:
-			return
-		# Lookup the request token
-		request_token = OAuthToken.gql("WHERE token_key=:key", key=oauth_token).get()
-		if request_token is None:
-			return
-		# Rebuild the auth handler
-		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-		auth.set_request_token(request_token.token_key, request_token.token_secret)
-		# Fetch the access token
-		try:
-			auth.get_access_token(oauth_verifier)
-		except tweepy.TweepError, e:
-			return
-		# So now we could use this auth handler.
-		# Here we will just display the access token key&secret
-		twitterapi = tweepy.API(auth)
-		user = twitterapi.me()
+    @property
+    def current_user_twitter(self):
+        self._current_user_twitter = None
+        oauth_token = self.request.get("oauth_token")
+        oauth_verifier = self.request.get("oauth_verifier")
+        if oauth_token is None:
+            return
+        # Lookup the request token
+        request_token = OAuthToken.gql("WHERE token_key=:key", key=oauth_token).get()
+        if request_token is None:
+            return
+        # Rebuild the auth handler
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_request_token(request_token.token_key, request_token.token_secret)
+        # Fetch the access token
+        try:
+            auth.get_access_token(oauth_verifier)
+        except tweepy.TweepError, e:
+            return
+        # So now we could use this auth handler.
+        # Here we will just display the access token key&secret
+        twitterapi = tweepy.API(auth)
+        user = twitterapi.me()
 
-		userb = User(key_name=user.screen_name,
-					id=user.screen_name,
-					name=user.screen_name,
-					profile_url=user.screen_name,
-					access_token=str(auth.access_token))
-		userb.put()
-		self._current_user_twitter = userb
+        userb = User(key_name=user.screen_name,
+                    id=user.screen_name,
+                    name=user.screen_name,
+                    profile_url=user.screen_name,
+                    access_token=str(auth.access_token))
+        userb.put()
+        self._current_user_twitter = userb
 
-		return self._current_user_twitter
+        return self._current_user_twitter
 
 
-	def get(self):
-		q_count = None
-		cutt = self.current_user_twitter
-		# Check is user is logged in (FB)
-		if self.current_user:
-			# Check if the user is already participating
-			uid = self.current_user.id
-			q_uid = db.GqlQuery("SELECT * FROM Posts WHERE id_autor = :1", uid)
-			q_count = q_uid.count()
-		else:
-			# Check if the user is logged in (TT)
-			if cutt:
-				if self.request.get("oauth_token") is not None:
-					# Set cookie
-					cookie = '%s|%s' % (cutt.id, cutt.access_token)
-					self.response.headers.add_header('Set-Cookie', 'startech=%s; expires=Fri, 31-Dec-2020 23:59:59 GMT' % cookie.encode())
-					self.redirect('/participa')
-					return
-			else:
-				# Read from cookie
-				startech = self.request.cookies.get('startech')
-				if startech is not None:
-					cookie_args = list()
-					cookie_args = split(startech, '|')
-					userb = User(key_name=cookie_args[0],
-							id=cookie_args[0],
-							name=cookie_args[0],
-							profile_url=cookie_args[0],
-							access_token=str(cookie_args[1]))
-					# Check if cookie data matches DB
-					checkuser = db.GqlQuery("SELECT * FROM User WHERE id = :1 AND access_token = :2", userb.id, userb.access_token)
-					if checkuser.count() == 1:
-						cutt = userb
-						# Check user posts
-						q_uid = db.GqlQuery("SELECT * FROM Posts WHERE id_autor = :1", userb.id)
-						q_count = q_uid.count()
-		# Get last 12 posts
-		q = db.GqlQuery("SELECT * FROM Posts ORDER BY fecha DESC")
-		last_posts = q.fetch(20)
-		for post in last_posts:
-			if post.id_autor.isdigit():
-				post.profile_img = 'http://graph.facebook.com/%s/picture' % (post.id_autor)
-			else:
-				post.profile_img = 'http://api.twitter.com/1/users/profile_image?screen_name=%s' % (post.id_autor)
-		# Render template
-		path = os.path.join(os.path.dirname(__file__), "participa.html")
-		args = dict(lang=set_lang_cookie_and_return_dict(self), current_user=self.current_user, current_user_twitter=cutt, facebook_app_id=FACEBOOK_APP_ID, authurl='/oauth/', last_posts=last_posts, already=q_count)
-		self.response.out.write(template.render(path, args))
+    def get(self):
+        q_count = None
+        cutt = self.current_user_twitter
+        # Check is user is logged in (FB)
+        if self.current_user:
+            # Check if the user is already participating
+            uid = self.current_user.id
+            q_uid = db.GqlQuery("SELECT * FROM Posts WHERE id_autor = :1", uid)
+            q_count = q_uid.count()
+        else:
+            # Check if the user is logged in (TT)
+            if cutt:
+                if self.request.get("oauth_token") is not None:
+                    # Set cookie
+                    cookie = '%s|%s' % (cutt.id, cutt.access_token)
+                    self.response.headers.add_header('Set-Cookie', 'startech=%s; expires=Fri, 31-Dec-2020 23:59:59 GMT' % cookie.encode())
+                    self.redirect('/participa')
+                    return
+            else:
+                # Read from cookie
+                startech = self.request.cookies.get('startech')
+                if startech is not None:
+                    cookie_args = list()
+                    cookie_args = split(startech, '|')
+                    userb = User(key_name=cookie_args[0],
+                            id=cookie_args[0],
+                            name=cookie_args[0],
+                            profile_url=cookie_args[0],
+                            access_token=str(cookie_args[1]))
+                    # Check if cookie data matches DB
+                    checkuser = db.GqlQuery("SELECT * FROM User WHERE id = :1 AND access_token = :2", userb.id, userb.access_token)
+                    if checkuser.count() == 1:
+                        cutt = userb
+                        # Check user posts
+                        q_uid = db.GqlQuery("SELECT * FROM Posts WHERE id_autor = :1", userb.id)
+                        q_count = q_uid.count()
+        # Get last 12 posts
+        q = db.GqlQuery("SELECT * FROM Posts ORDER BY fecha DESC")
+        last_posts = q.fetch(12)
+        for post in last_posts:
+            if post.id_autor.isdigit():
+                post.profile_img = 'http://graph.facebook.com/%s/picture' % (post.id_autor)
+            else:
+                post.profile_img = 'http://api.twitter.com/1/users/profile_image?screen_name=%s' % (post.id_autor)
+        # Render template
+        path = os.path.join(os.path.dirname(__file__), "participa.html")
+        args = dict(lang=set_lang_cookie_and_return_dict(self), current_user=self.current_user, current_user_twitter=cutt, facebook_app_id=FACEBOOK_APP_ID, authurl='/oauth/', last_posts=last_posts, already=q_count)
+        self.response.out.write(template.render(path, args))
 
-	def post(self):
-		q_count = None
-		# Check is user is logged in
+    def post(self):
+        """
+        Write post
+        """
+        # Check is user is logged in
         # FACEBOOK
-		if self.current_user:
-			# Check if the user is already participating
-			uid = self.current_user.id
-			q_uid = db.GqlQuery("SELECT * FROM Posts WHERE id_autor = :1", uid)
-			q_count = q_uid.count()
-			if not q_count:
-				# Get Form data
-				msg = self.request.str_POST["content"]
-				msg = unicode(msg, 'utf-8')
-				# Check length restrictions
-				if len(msg) > 140:
-					self.redirect('/participa')
-					return
-				elif len(msg) < 5:
-					self.redirect('/participa')
-					return
-				else:
-					graph = facebook.GraphAPI(self.current_user.access_token)
-					profile = graph.get_object("me")
-					new_post = Posts(id_autor = profile["id"], nombre = profile["name"], msj = msg, network = "facebook")
-					new_post.put()
-					fb_response = graph.put_wall_post(msg.encode('utf-8'))
-		else:
+        if self.current_user:
+            # Get Form data
+            msg = self.request.str_POST["content"]
+            msg = unicode(msg, 'utf-8')
+
+            graph = facebook.GraphAPI(self.current_user.access_token)
+            profile = graph.get_object("me")
+            new_post = Posts(id_autor = profile["id"], nombre = profile["name"], msj = msg, network = "facebook")
+            new_post.put()
+            fb_response = graph.put_wall_post(msg.encode('utf-8'))
+        else:
+            # Check is user is logged in
             # TWITTER
-			cutt = None
-			# Read from cookie
-			startech = self.request.cookies.get('startech')
-			if startech is not None:
-				cookie_args = list()
-				cookie_args = split(startech, '|')
-				userb = User(key_name=cookie_args[0],
-						id=cookie_args[0],
-						name=cookie_args[0],
-						profile_url=cookie_args[0],
-						access_token=str(cookie_args[1]))
-				# Check if cookie data matches DB
-				checkuser = db.GqlQuery("SELECT * FROM User WHERE id = :1 AND access_token = :2", userb.id, userb.access_token)
-				if checkuser.count() == 1:
-					cutt = userb
-			if cutt:
-				# Check if the user is already participating
-				uid = cutt.id
-				q_uid = db.GqlQuery("SELECT * FROM Posts WHERE id_autor = :1", uid)
-				q_count = q_uid.count()
-				if not q_count:
-					# Get Form data
-					msg = self.request.str_POST["content"]
-					msg = unicode(msg, 'utf-8')
-					# Check length restrictions
-					if len(msg) > 140:
-						self.redirect('/participa')
-						return
-					elif len(msg) < 5:
-						self.redirect('/participa')
-						return
-					else:
-						msg += ' #startechconf'
-						# Normally the key and secret would not be passed but rather
-						# stored in a DB and fetched for a user.
-						tokens = list()
-						tokens = split(userb.access_token, '&')
-						token_key = tokens[1].replace("oauth_token=", "")
-						token_secret = tokens[0].replace("oauth_token_secret=", "")
-						#Here we authenticate this app's credentials via OAuth
-						auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-						#Here we set the credentials that we just verified and passed in.
-						auth.set_access_token(token_key, token_secret)
-						#Here we authorize with the Twitter API via OAuth
-						twitterapi = tweepy.API(auth)
-						#Here we update the user's twitter timeline with the tweeted text.
-						twitterapi.update_status(msg.encode('utf-8'))
-						new_post = Posts(id_autor = cutt.id, nombre = cutt.name, msj = msg, network = "facebook")
-						new_post.put()
-		self.redirect('/participa')
+            cutt = None
+            # Read from cookie
+            startech = self.request.cookies.get('startech')
+            if startech is not None:
+                cookie_args = list()
+                cookie_args = split(startech, '|')
+                userb = User(key_name=cookie_args[0],
+                        id=cookie_args[0],
+                        name=cookie_args[0],
+                        profile_url=cookie_args[0],
+                        access_token=str(cookie_args[1]))
+                # Check if cookie data matches DB
+                checkuser = db.GqlQuery("SELECT * FROM User WHERE id = :1 AND access_token = :2", userb.id, userb.access_token)
+                if checkuser.count() == 1:
+                    cutt = userb
+            if cutt:
+                # Get Form data
+                msg = self.request.str_POST["content"]
+                msg = unicode(msg, 'utf-8')
+                # Check length restrictions
+                if len(msg) > 140:
+                    self.redirect('/participa')
+                    return
+                elif len(msg) < 5:
+                    self.redirect('/participa')
+                    return
+                else:
+                    msg += ' #startechconf'
+                    # Normally the key and secret would not be passed but rather
+                    # stored in a DB and fetched for a user.
+                    tokens = list()
+                    tokens = split(cutt.access_token, '&')
+                    token_key = tokens[1].replace("oauth_token=", "")
+                    token_secret = tokens[0].replace("oauth_token_secret=", "")
+                    # Here we authenticate this app's credentials via OAuth
+                    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+                    # Here we set the credentials that we just verified and passed in.
+                    auth.set_access_token(token_key, token_secret)
+                    # Here we authorize with the Twitter API via OAuth
+                    twitterapi = tweepy.API(auth)
+                    # Here we update the user's twitter timeline with the tweeted text.
+                    twitterapi.update_status(msg.encode('utf-8'))
+                    new_post = Posts(id_autor = cutt.id, nombre = cutt.name, msj = msg, network = "twitter")
+                    new_post.put()
+        self.redirect('/participa')
 
 def main():
     util.run_wsgi_app(webapp.WSGIApplication([
-		(r'/oauth/', TwitterOauth),
-		(r'/salir/', TwitterLogout),
-		(r"/participa", HomeHandler)
-	]))
+        (r'/oauth/', TwitterOauth),
+        (r'/salir/', TwitterLogout),
+        (r"/participa", HomeHandler)
+    ], debug=False))
 
 
 if __name__ == "__main__":
